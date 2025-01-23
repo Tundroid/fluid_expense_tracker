3c8fb24c005ca5aa48424391d5df2d5a5063a0a7
Felix Kimbu <felixkimbu@outlook.com>
2025-01-23 08:30:25 +0100

    cleanup...

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,64 +5,25 @@ Contains the class DBStorage
 
 import models
 from models.base_model import Database
-from models.depot_detail import DepotDetail
-from models.operation import Operation
-from models.family import Family
-from models.item_cat import ItemCategory
-from models.sector import Sector
-from models.deposit_detail import DepositDetail
-from models.deposit import Deposit
-from models.supplier_contact import SupplierContact
-from models.supplier_type import SupplierType
-from models.supplier import Supplier
-from models.supply_detail import SupplyDetail
-from models.supply import Supply
-from models.pay_mode import PayMode
-from models.depot_map import DepotMap
-from models.depot import Depot
-from models.inventory_snapshot import InventorySnapshot
-from models.item import Item
-from models.barcode import Barcode
-from models.client_account import ClientAccount
-from models.account_type import AccountType
-from models.account import Account
-from models.user_account import UserAccount
-from models.record_detail import RecordDetail
-from models.record import Record
-from models.payment import Payment
+from models.budget import Budget
+from models.category import Category
+from models.expense import Expense
+from models.income import Income
+from models.saving_goal import SavingGoal
+from models.saving import Saving
+from models.user import User
 from os import getenv
 from sqlalchemy import create_engine, inspect, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-classes_commerce = {
-    "barcode": Barcode,
-    "client_account": ClientAccount,
-    "deposit": Deposit,
-    "deposit_detail": DepositDetail,
-    "depot": Depot,
-    "depot_detail": DepotDetail,
-    "depot_map": DepotMap,
-    "family": Family,
-    "inventory_snapshot": InventorySnapshot,
-    "item": Item,
-    "item_cat": ItemCategory,
-    "operation": Operation,
-    "pay_mode": PayMode,
-    "payment": Payment,
-    "record": Record,
-    "record_detail": RecordDetail,
-    "sector": Sector,
-    "supplier": Supplier,
-    "supplier_contact": SupplierContact,
-    "supplier_type": SupplierType,
-    "supply": Supply,
-    "supply_detail": SupplyDetail,
-    "user_account": UserAccount,
-}
-
-classes_account = {
-    "account": Account,
-    "account_type": AccountType
+classes = {
+    "budger": Budget,
+    "category": Category,
+    "expense": Expense,
+    "income": Income,
+    "saving_goal": SavingGoal,
+    "saving": Saving,
+    "user": User
 }
 
 
